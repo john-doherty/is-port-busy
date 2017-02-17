f11be6b5d3cb9f715cd00efdd962ffe7c3bc5fa0
John Doherty <doherty.j@cambridgeassessment.org.uk>
2017-02-17 11:20:38 +0000

    added unit test to check when port is busy

--- test/isportbusy.spec.js
+++ test/isportbusy.spec.js
@@ -1,11 +1,28 @@
 'use strict';
 
+var http = require('http');
 var isPortBusy = require('../');
+var server = null;
+var port = 8888;
 
 describe('isPortBusy', function () {
 
+    beforeEach(function (done) {
+        server = http.createServer();
+        server.listen(port, done);
+    });
+
     it('should be defined', function () {
+
         expect(isPortBusy).toBeDefined();
     });
 
+    it('should execute .then when port is busy', function (done) {
+
+        isPortBusy(port).then(done);
+    });
+
+    afterEach(function (done) {
+        server.close(done);
+    });
 });
