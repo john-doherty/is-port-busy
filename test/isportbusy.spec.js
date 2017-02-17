59c33883ada87f8271cee8ef4adfe7e98ad28882
John Doherty <doherty.j@cambridgeassessment.org.uk>
2017-02-17 10:31:20 +0000

    cleaned up project and added test framework

---
+++ test/isportbusy.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var isPortBusy = require('../');
+
+describe('isPortBusy', function () {
+
+    it('should be defined', function () {
+        expect(isPortBusy).toBeDefined();
+    });
+
+});
